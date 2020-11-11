26535cb9fb7ad319d3bef84d31563e873f9fefaa
Ariful4593 <arifkhagoria2015@gmail.com>
2020-11-11 23:28:22 +0600

    Backend code updated

--- src/components/Home/Doctors/Doctors.js
+++ src/components/Home/Doctors/Doctors.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Doctor from '../Doctor/Doctor';
+import { useState } from 'react';
+
 const Doctors = () => {
+    const [doctors, setDoctors] = useState([])
+    useEffect( () =>{
+        fetch('http://localhost:4000/doctors')
+        .then(res => res.json())
+        .then(data => setDoctors(data))
+    }, [])
+    console.log(doctors)
     return (
         <section className="doctors">
             <div className="container">
                 <h5 className="text-center  text-primary mb-5">Our Doctors</h5>
                 <div className="row">
-                    <Doctor/>
-                    <Doctor/>
-                    <Doctor/>
+                    {
+                        doctors.map(doctor => <Doctor doctor={doctor}></Doctor>)
+                    }
                 </div>
             </div>
         </section>
